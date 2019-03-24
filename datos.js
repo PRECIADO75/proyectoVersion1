7d081aba3d76aea1b0e58c9bc32e1c0907eb5e3d
preciado75 <preciado75@yahoo.com>
2019-03-23 19:47:36 -0500

    proyectoVesion1

--- datos.js
+++ datos.js
@@ -1,8 +1,6 @@
 const opciones={
     matematicas:{ default:0,alias:'m'},
-    ingles:{default:0,alias:'i'},
-    programacion:{default:0,alias:'p'},
-    nombre:{demand:true,alias:'m'}
+    ingles:{default:0,alias:'i'}
 };
 const argv = require('yargs').command('promedio','calcular promedio',opciones).argv;
 var obtenerPromedio=(argv.matematicas + argv.ingles + argv.programacion)/3;
